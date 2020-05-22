fee2fa4e2373658b1117b6b93e2358da1235b8e4
Paramtamtam <slashkot@gmail.com>
2020-05-22 19:14:56 +0500

    Tests bottstrap fix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,6 @@
 
 declare(strict_types=1);
 
-ini_set('error_reporting', E_ALL);
+ini_set('error_reporting', (string) \E_ALL);
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
