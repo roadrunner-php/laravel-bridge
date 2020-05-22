38d4f09ef32a4170dbc9d89e29cad5ba9e238e5b
Paramtamtam <tarampampam@users.noreply.github.com>
2020-05-22 18:26:40 +0500

    Basic repo files added (#1)

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# TODO
+
+> Will be removed before first release
+
+- [ ] Get codecov token for repo (`CODECOV_TOKEN`)
+- [ ] Refactor composer scripts (like `test-cover`)
---
+++ src/ServiceProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Spiral\RoadRunnerLaravel;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    //
+}
