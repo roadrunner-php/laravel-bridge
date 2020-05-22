6a9fe8da8cb22c30063862ab8f7b1f6bdd6705d1
Paramtamtam <slashkot@gmail.com>
2020-05-22 17:32:51 +0500

    Basic repo files added

---
+++ tests/ServiceProviderTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Spiral\RoadRunnerLaravel\Tests;
+
+/**
+ * @covers \Spiral\RoadRunnerLaravel\ServiceProvider<extended>
+ */
+class ServiceProviderTest extends \Illuminate\Foundation\Testing\TestCase
+{
+    //
+}
