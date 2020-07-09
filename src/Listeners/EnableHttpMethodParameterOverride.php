9141108c9ccbb3359d435f9664e8e1f111467485
assurrussa <assurrussa@yandex.ru>
2020-07-09 11:43:27 +0500

    Refactor fixed ignore _method

--- src/Listeners/EnableHttpMethodParameterOverride.php
+++ src/Listeners/EnableHttpMethodParameterOverride.php
@@ -8,7 +8,8 @@ use Spiral\RoadRunnerLaravel\Events\Contracts\WithHttpRequest;
 
 /**
  * Class EnableHttpMethodParameterOverride
- * This listener must be registered BEFORE `RebindRouterListener` if you need to process the hidden `_hidden` field when submitted using the HTML form
+ * This listener must be registered BEFORE `RebindRouterListener` if you need to process
+ * the hidden `_hidden` field when submitted using the HTML form.
  *
  * @see RebindRouterListener
  * @package Spiral\RoadRunnerLaravel\Listeners
