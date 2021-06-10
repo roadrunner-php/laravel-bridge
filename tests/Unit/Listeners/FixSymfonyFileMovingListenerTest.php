39f03ddef2de9f4bc8d6d0875a203e073e22828c
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2021-06-11 00:46:33 +0500

    Update test

--- tests/Unit/Listeners/FixSymfonyFileMovingListenerTest.php
+++ tests/Unit/Listeners/FixSymfonyFileMovingListenerTest.php
@@ -17,19 +17,19 @@ class FixSymfonyFileMovingListenerTest extends AbstractListenerTestCase
      */
     public function testHandle(): void
     {
+        $tmp_dir = $this->createTemporaryDirectory();
+
         $function_location = '\\Symfony\\Component\\HttpFoundation\\File\\move_uploaded_file';
+        $old_file_path = $tmp_dir . DIRECTORY_SEPARATOR . Str::random();
+        $new_file_path = $tmp_dir . DIRECTORY_SEPARATOR . Str::random();
+        \file_put_contents($old_file_path, '');
 
         $this->assertFalse(\function_exists($function_location));
-        $this->assertFalse(\is_uploaded_file('foo'));
+        $this->assertFalse(\move_uploaded_file($old_file_path, $new_file_path));
 
         $this->listenerFactory()->handle(new \stdClass());
 
         $this->assertTrue(\function_exists($function_location));
-
-        $tmp_dir = $this->createTemporaryDirectory();
-        $old_file_path = $tmp_dir . DIRECTORY_SEPARATOR . Str::random();
-        $new_file_path = $tmp_dir . DIRECTORY_SEPARATOR . Str::random();
-        \file_put_contents($old_file_path, '');
         $this->assertFileExists($old_file_path);
         $this->assertTrue($function_location($old_file_path, $new_file_path));
         $this->assertFileExists($new_file_path);
@@ -37,6 +37,8 @@ class FixSymfonyFileMovingListenerTest extends AbstractListenerTestCase
         $rnd_file_path1 = $tmp_dir . DIRECTORY_SEPARATOR . Str::random();
         $rnd_file_path2 = $tmp_dir . DIRECTORY_SEPARATOR . Str::random();
         $this->assertFalse($function_location($rnd_file_path1, $rnd_file_path2));
+        $this->assertFileNotExists($rnd_file_path1);
+        $this->assertFileNotExists($rnd_file_path2);
     }
 
     /**
