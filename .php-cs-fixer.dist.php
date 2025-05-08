b5fedc0b869dad2503123daf5609084a4917a474
roxblnfk <roxblnfk@ya.ru>
2025-05-08 15:39:47 +0400

    Add CS fixer CI workflow

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -10,4 +10,5 @@ return \Spiral\CodeStyle\Builder::create()
     ->include(__DIR__ . '/config')
     ->include(__DIR__ . '/tests')
     ->include(__DIR__ . '/rector.php')
+    ->allowRisky(false)
     ->build();
