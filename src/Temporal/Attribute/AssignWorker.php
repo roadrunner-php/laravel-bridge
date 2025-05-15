992282c4fa1daa1b544f7e2bc23868583d1be726
butschster <butschster@gmail.com>
2025-05-15 17:41:56 +0200

    feat: Add NamedArgumentConstructor to AssignWorker attribute

--- src/Temporal/Attribute/AssignWorker.php
+++ src/Temporal/Attribute/AssignWorker.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Spiral\RoadRunnerLaravel\Temporal\Attribute;
 
-#[\Attribute(\Attribute::TARGET_CLASS | \Attribute::IS_REPEATABLE)]
+use Doctrine\Common\Annotations\Annotation\NamedArgumentConstructor;
+
+#[\Attribute(\Attribute::TARGET_CLASS | \Attribute::IS_REPEATABLE), NamedArgumentConstructor]
 final readonly class AssignWorker
 {
     /**
